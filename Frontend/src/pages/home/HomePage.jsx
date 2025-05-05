e962742e002594c7e6f14179fbc9e421819abe40
Sourav Prajapath <souravprajapath257@gmail.com>
2025-05-05 20:17:09 +0530

    Frontend

---
+++ Frontend/src/pages/home/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AuthScreen from './AuthScreen';
+import HomeScreen from './HomeScreen';
+import { useAuthStore } from '../../store/authUser';
+
+const HomePage = () => {
+
+  const {user} = useAuthStore();
+
+  return (
+    <>
+      {user ? < HomeScreen /> : < AuthScreen />}
+    </>
+  )
+}
+
+export default HomePage
